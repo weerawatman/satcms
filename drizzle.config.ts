86d83ee6bc29bdb8e2f6a983a2a2818373ba2dfa
Weerawat Mansathit <Weerawat.m@somboon.co.th>
2026-02-05 17:35:28 +0700

    Fix env loading and dev script

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig } from "drizzle-kit"
+import dotenv from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+dotenv.config({ path: ".env.local" });
 
 export default defineConfig({
     schema: "./src/db/schema.ts",
@@ -6,5 +9,5 @@ export default defineConfig({
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
-    }
-})
+    },
+});
